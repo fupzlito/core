be43a40df4ec25025862cd3df13a1fdb25ac4838
Anton Kharitonov <fupzlito@icloud.com>
2026-01-19 02:02:51 -0500

    add missing COPR repos

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,11 +3,22 @@
 set -ouex pipefail
 
 ### Install packages
-dnf5 install -y docker docker-compose-plugin qemu-guest-agent tailscale ctop samba nfs-utils
+dnf5 install -y docker qemu-guest-agent tailscale samba nfs-utils
+
+
+
+dnf5 -y copr enable rhcontainerbot/docker-compose
+dnf5 -y copr enable bcicen/ctop
+
+dnf5 install -y docker-compose-plugin
+dnf5 -y install ctop
+
+dnf5 -y copr disable rhcontainerbot/docker-compose
+dnf5 -y copr disable bcicen/ctop
+
+dnf5 clean all
+rm -rf /var/cache/dnf
 
 ### Set systemd
 systemctl enable docker qemu-guest-agent tailscaled
 systemctl disable --now podman.socket || true
-
-dnf5 clean all
-rm -rf /var/cache/dnf
