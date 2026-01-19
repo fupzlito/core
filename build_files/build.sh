c391c84643cb265e2ddcdb4361d64147e5ef1f52
Anton Kharitonov <fupzlito@icloud.com>
2026-01-19 02:15:00 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,14 @@
 set -ouex pipefail
 
 ### Install packages
-dnf5 install -y docker qemu-guest-agent tailscale samba nfs-utils
+dnf5 install -y docker docker-compose-plugin docker-buildx-plugin qemu-guest-agent tailscale samba nfs-utils
 dnf5 install -y 'dnf5-command(copr)'
 
 
 dnf5 -y copr enable rhcontainerbot/docker-compose
 dnf5 -y copr enable bcicen/ctop
 
-dnf5 install -y docker-compose-plugin ctop
+dnf5 install -y docker-compose ctop
 
 dnf5 -y copr disable rhcontainerbot/docker-compose
 dnf5 -y copr disable bcicen/ctop
