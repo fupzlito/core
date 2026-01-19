bbd5ef41e96d638623e43a46c7ea9d142816f25f
Anton Kharitonov <fupzlito@icloud.com>
2026-01-19 06:30:11 -0500

    install ignition

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,7 @@
 set -ouex pipefail
 
 # Install all required packages
-dnf5 install -y docker docker-compose qemu-guest-agent tailscale samba nfs-utils curl
+dnf5 install -y ignition docker docker-compose qemu-guest-agent tailscale samba nfs-utils curl
 
 # Install ctop static binary
 curl -Lo /usr/local/bin/ctop https://github.com/bcicen/ctop/releases/download/v0.9.8/ctop-0.9.8-linux-amd64
@@ -15,4 +15,4 @@ rm -rf /var/cache/dnf
 # Systemd services
 systemctl mask systemd-journald-audit.socket
 systemctl disable --now podman.socket || true
-systemctl enable docker qemu-guest-agent tailscaled
+systemctl enable ignition docker qemu-guest-agent tailscaled
