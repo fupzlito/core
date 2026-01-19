40985ad0b1b2a77857db70cd236b1ebcbe6d3585
Anton Kharitonov <fupzlito@icloud.com>
2026-01-19 02:06:27 -0500

    add COPR plugin

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,13 @@ set -ouex pipefail
 
 ### Install packages
 dnf5 install -y docker qemu-guest-agent tailscale samba nfs-utils
-
+dnf5 install -y 'dnf5-command(copr)'
 
 
 dnf5 -y copr enable rhcontainerbot/docker-compose
 dnf5 -y copr enable bcicen/ctop
 
-dnf5 install -y docker-compose-plugin
-dnf5 -y install ctop
+dnf5 install -y docker-compose-plugin ctop
 
 dnf5 -y copr disable rhcontainerbot/docker-compose
 dnf5 -y copr disable bcicen/ctop
