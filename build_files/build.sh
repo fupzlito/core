4114bb3c0ad7104d6cb1e99bf645fe192a5a88ef
Anton Kharitonov <fupzlito@icloud.com>
2026-01-20 23:25:40 -0500

    fixes

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,9 +33,3 @@ dnf5 -y makecache
 curl -Lo /usr/local/bin/ctop \
      -L https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64
 chmod +x /usr/local/bin/ctop
-
-
-#ln -s /usr/lib/systemd/system/cloud-init-local.service /etc/systemd/system/multi-user.target.wants/cloud-init-local.service
-#ln -s /usr/lib/systemd/system/cloud-init.service /etc/systemd/system/multi-user.target.wants/cloud-init.service
-#ln -s /usr/lib/systemd/system/cloud-config.service /etc/systemd/system/multi-user.target.wants/cloud-config.service
-#ln -s /usr/lib/systemd/system/cloud-final.service /etc/systemd/system/multi-user.target.wants/cloud-final.service
